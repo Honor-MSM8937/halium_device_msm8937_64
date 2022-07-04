b9c0da2941879d817e97a961ac3616b32b78b674
KrutosX <52695559+KrutosVIP@users.noreply.github.com>
2022-07-04 21:51:59 +0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -85,6 +85,8 @@ fi
 
 "$SCRIPT/make-bootimage.sh" "${TMPDOWN}/KERNEL_OBJ" "${TMPDOWN}/halium-boot-ramdisk.img" "${TMP}/partitions/boot.img"
 
+cp "${TMP}/partitions/boot.img" "${OUT}"
+
 cp -av overlay/* "${TMP}/"
 "$SCRIPT/build-tarball-mainline.sh" "${deviceinfo_codename}" "${OUT}" "${TMP}"
 
