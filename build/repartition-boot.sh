7301a39d69e06be21eae88bf492dcd8ba82f742f
KrutosX <52695559+KrutosVIP@users.noreply.github.com>
2022-07-05 04:30:19 +0400

    Replace sh with bash

--- build/repartition-boot.sh
+++ build/repartition-boot.sh
@@ -5,7 +5,7 @@ echo "! Ubuntu only script for gh-actions."
 
 echo "> Getting Images Info..."
 echo "-> Getting all deps and AIK from SebaUbuntu/AIK-Linux-mirror..."
-apt update -y && apt install git gzip lzop lzma tar bzip2 lz4 xz-utils cpio wget sudo file -y # Check up git
+sudo apt update -y && sudo apt install git gzip lzop lzma tar bzip2 lz4 xz-utils cpio wget sudo file -y # Check up git
 git clone https://github.com/SebaUbuntu/AIK-Linux-mirror aik-linux
 cd aik-linux
 
@@ -14,24 +14,24 @@ wget -O ramdisk.img $RAMDISK_IMAGE && wget -O kernel.img $KERNEL_IMAGE
 
 echo "-> Unpacking boot image..."
 cp $BOOT_IMAGE ./boot.img
-sh unpackimg.sh ./boot.img
+bash unpackimg.sh ./boot.img
 mkdir ../target && mv ramdisk split_img ../target 
 
 echo "-> Repacking ramdisk.img..."
-sh cleanup.sh && sh unpackimg.sh ./ramdisk.img # Unpacking ramdisk
+bash cleanup.sh && bash unpackimg.sh ./ramdisk.img # Unpacking ramdisk
 
 cp ../target/split_img/boot.img-cmdline ./split_img/ramdisk.img-cmdline # Installing our options and ramdisk
 rm -rf ./ramdisk && cp ../target/ramdisk ./ramdisk -r 
-sh repackimg.sh && mv image-new.img ../target/ramdisk.img
+bash repackimg.sh && mv image-new.img ../target/ramdisk.img
 sh cleanup.sh
 
 echo "-> Repacking kernel.img..."
-sh cleanup.sh && sh unpackimg.sh ./kernel.img
+bash cleanup.sh && bash unpackimg.sh ./kernel.img
 
 cp ../target/split_img/boot.img-cmdline ./split_img/kernel.img-cmdline # Installing our options and kernel
 cp ../target/split_img/boot.img-kernel ./split_img/kernel.img-kernel 
-sh repackimg.sh && mv image-new.img ../target/kernel.img
-sh cleanup.sh && rm -rf ../target/split_img ../target/ramdisk
+bash repackimg.sh && mv image-new.img ../target/kernel.img
+bash cleanup.sh && rm -rf ../target/split_img ../target/ramdisk
 cd ..
 
 echo "> Done; Uploading images..."
